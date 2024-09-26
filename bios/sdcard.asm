cc83f7c66b47dc4fc231bf5ed78655e019c722ca
VGamanin <hplus.foxtail@gmail.com>
2024-09-26 13:24:52 +0300

    Card Detector

--- bios/sdcard.asm
+++ bios/sdcard.asm
@@ -10,15 +10,100 @@ sdread:
         call    sdinit
 .s1:    ret
 
-sdinit:
+; ----------------------------------------------------------------------
+; Инициализация SD карты
+; ----------------------------------------------------------------------
 
-        xor     a
-        call    sdcmd           ; 80T
+sdinit: xor     a
+        ld      (SD_TYPE), a
+        call    sdcmd               ; 80T
         ld      hl, $0000
         ld      (SD_ARG32), hl
         ld      (SD_ARG32+2), hl
-        ld      a, 0            ; CMD0
+        ld      a, 0                ; CMD0
         call    sdcomm
+        ret     c
+        cp      1                   ; Должен быть R1_IDLE код в ответе
+        jr      z, .a1
+        scf
+        ret
+
+        ; Запросить CMD8 с аргументом $01AA
+.a1:    ld      hl, $01AA
+        ld      (SD_ARG32), hl
+        ld      a, 8                ; CMD8
+        call    sdcomm
+        ret     c
+        bit     2, a                ; & R1_ILLEGAL_COMMAND ?
+        jr      z, .a2
+        ld      a, 1                ; Если да, то это SD1
+        ld      (SD_TYPE), a        ; SD1
+        jr      .acmd
+.a2:    ld      b, 4
+.a3:    call    sdget
+        djnz    .a3
+        cp      $AA
+        ld      a, 2
+        ld      (SD_TYPE), a
+        jr      z, .acmd            ; На 4-м байте должен вернуть ответ AAh
+        scf
+        ret
+
+        ; Отослать ACMD
+.acmd:  ld      hl, 4095
+.a7:    push    hl
+        ld      hl, $0000
+        ld      (SD_ARG32), hl
+        ld      (SD_ARG32+2), hl
+        ld      a, 55
+        call    sdcomm
+
+        ; IF SDType=2 THEN $40 ELSE $00
+        ld      a, (SD_TYPE)
+        cp      2
+        ld      a, 0x00
+        jr      nz, .a5
+        ld      a, 0x40
+.a5:    ld      (SD_ARG32+3), a
+        ld      a, 0x29
+        call    sdcomm
+        pop     hl
+        ret     c                   ; Возникла ошибка
+        and     a                   ; = R1_READY_STATE?
+        jr      z, .a6
+        dec     hl
+        ld      a, h
+        or      l
+        jr      nz, .a7
+        scf
+        ret                         ; От ACMD ответа не было
+
+        ; Если SD2, то запросить SDHC
+.a6:    ld      a, (SD_TYPE)
+        cp      2
+        jr      nz, .end
+
+        ld      hl, $0000
+        ld      (SD_ARG32), hl
+        ld      (SD_ARG32+2), hl
+        ld      a, 58               ; CMD58
+        call    sdcomm
+        ret     c
+
+        ; Определить что это SDHC по наличию 2х бит в 7 и 6 битах
+        call    sdget
+        and     $c0
+        cp      $c0
+        jr      nz, .a8
+        ld      a, 3
+        ld      (SD_TYPE), a        ; SDHC карта
+.a8:    call    sdget               ; Дочитать 3 байта
+        call    sdget
+        call    sdget
+.end:   ld      a, 3
+        call    sdcmd               ; CE=1
+        scf
+        ccf
         ret
 
 ; ----------------------------------------------------------------------
@@ -38,7 +123,6 @@ sdcomm: ld      (TMP16), a      ; Сохранить команду
         ld      a, h
         or      l
         jr      nz, .k0
-        ld      a, SD_ERR1
         scf
         ret
 
@@ -73,13 +157,11 @@ sdcomm: ld      (TMP16), a      ; Сохранить команду
         jp      p, .k5
         dec     h
         jr      nz, .k4
-        ld      a, SD_ERR2
         scf
         ret
 
         ; CF=0, функция в порядке, ответ -> A
-.k5:    halt
-        scf
+.k5:    scf
         ccf
         ret
 
